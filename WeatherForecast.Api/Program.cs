80acdcd71c87a11eff331f1139dddea11284923f
John Miller <john.miller@unionstreet.co.uk>
2024-01-22 11:49:54 +0000

    Initiate the swagger index page

--- WeatherForecast.Api/Program.cs
+++ WeatherForecast.Api/Program.cs
@@ -12,12 +12,9 @@ builder.Services.AddSwaggerGen(c =>
 });
  
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+ 
+app.UseSwagger();
+app.UseSwaggerUI(); 
 
 app.UseHttpsRedirection();
 
